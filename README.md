c32284a6028775fe998e104184ff9e6ce9840111
Todor Iliev <todor@itprism.com>
2014-12-12 17:58:00 +0200

    version 1.6

--- README.md
+++ README.md
@@ -1,22 +1,30 @@
 ITPGads Module for Joomla!
 ==========================
-( Version 1.5 )
+( Version 1.6 )
 --------------------------
 
 This is a Joomla! module that adds Google AdSense ads on your website.
 
+##Documentation
+You can find documentation on following page.
+
+[Documentation and FAQ] (http://itprism.com/help/104-google-adsense-documentation)
+
+##Download
+You can [download ITP Gads package] (http://itprism.com/free-joomla-extensions/ecommerce-gamification/google-adsense-integration) from the website of ITPrism.
+
 Changelog
 ---------
 
-###v1.5
+###v1.6
+* Added option "Ad Type". Now, it is possible to choose between synchronous or asynchronous ad types.
 
+###v1.5
 * Renamed to ITPGads.
 
 ###v1.3
-
 * Improved translation
 * Added new kinds of ads - "Large Leaderboard ", "Large Skyscraper" 
 
 ###v1.2
-
 * Improved 
--- mod_itpgads.php
+++ mod_itpgads.php
@@ -3,64 +3,78 @@
  * @package      ITPGads
  * @subpackage   Modules
  * @author       Todor Iliev
- * @copyright    Copyright (C) 2013 Todor Iliev <todor@itprism.com>. All rights reserved.
+ * @copyright    Copyright (C) 2014 Todor Iliev <todor@itprism.com>. All rights reserved.
  * @license      http://www.gnu.org/copyleft/gpl.html GNU/GPL
  */
 
-defined( '_JEXEC' ) or die;
-
-$app = JFactory::getApplication();
-/** @var $app JSite **/
+defined('_JEXEC') or die;
 
 $moduleClassSfx = htmlspecialchars($params->get('moduleclass_sfx', ""));
 
+$app = JFactory::getApplication();
+/** @var $app JApplicationSite */
+
 // Get remote address
-$remoteAddress  = $app->input->server->get("REMOTE_ADDR");
+$remoteAddress = $app->input->server->get("REMOTE_ADDR");
 
 // Get blocked addresses
 $ips = explode(",", $params->get('blockedIPs'));
-foreach($ips as &$ip) {
+foreach ($ips as &$ip) {
     $ip = trim($ip);
 }
 
-$html = '<div class="itp-gads-'.$moduleClassSfx.'">';
+$html = '<div class="itp-gads' . $moduleClassSfx . '">';
 
 if (!in_array($remoteAddress, $ips)) {
-    
-    $customCode     = $params->get('custom_code');
-    
-    if(!empty($customCode)) { // Display the custom code
-    
+
+    $customCode = $params->get('custom_code');
+
+    if (!empty($customCode)) { // Display the custom code
+
         $html .= $customCode;
-    
+
     } else {
-        
-        $publisherId    = $params->get('publisherId');
-        $slotId         = $params->get('slot');
-        $channelId      = $params->get('channel');
-        
+
+        $publisherId = $params->get('publisherId');
+        $slotId      = $params->get('slot');
+        $channelId   = $params->get('channel');
+
         // Get size
-        $adFormat   = $params->get('format');
-        $format     = explode("-", $adFormat);
-        $width      = explode("x", $format[0]);
-        $height     = explode("_", $width[1]);
-        
-        $html .=  '
-    <script type="text/javascript"><!--
-        google_ad_client = "' . $publisherId . '";
-        google_ad_slot = "' . $slotId . '";
-        google_ad_width = ' . $width[0] . ';
-        google_ad_height = ' . $height[0] . ';
-        //-->
-        </script>
-        <script type="text/javascript"
-        src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-    </script>';
-        
+        $adFormat = $params->get('format');
+        $format   = explode("-", $adFormat);
+        $width    = explode("x", $format[0]);
+        $height   = explode("_", $width[1]);
+
+        if ($params->get("ad_type", 1)) { // Asynchronous
+
+            $html .= '
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:inline-block;width:' . $width[0] . 'px;height:' . $height[0] . 'px"
+     data-ad-client="' . $publisherId . '"
+     data-ad-slot="' . $slotId . '"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>';
+
+        } else { // Synchronous
+
+            $html .= '
+<script type="text/javascript"><!--
+    google_ad_client = "' . $publisherId . '";
+    google_ad_slot = "' . $slotId . '";
+    google_ad_width = ' . $width[0] . ';
+    google_ad_height = ' . $height[0] . ';
+    //-->
+    </script>
+    <script type="text/javascript"
+    src="//pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>';
+        }
     }
 
 } else {
-    $html .=  $params->get('altMessage');
+    $html .= $params->get('altMessage');
 }
 
 $html .= "</div>";
